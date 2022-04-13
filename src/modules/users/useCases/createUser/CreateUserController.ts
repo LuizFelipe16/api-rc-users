48817fb8fcad8f1a3c73b5b634314aa65989bcec
Luiz Felipe <felipefelizatti215@gmail.com>
2022-04-13 01:33:48 -0300

    solution

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -12,7 +12,9 @@ class CreateUserController {
       const user = this.createUserUseCase.execute({ name, email });
 
       return response.status(201).json(user);
+
     } catch (err) {
+
       return response.status(400).json({
         error: "error message!"
       });
